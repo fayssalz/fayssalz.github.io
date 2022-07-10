4a74a6359c90b331591a2a2985055efe909e7e4a
fayssalz <fayssalz@users.noreply.github.com>
2022-07-10 19:21:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -428,11 +428,6 @@ var Particle = function(canvas) {
     };
 };
 
-window.onload = function(){
-    document.getElementById("mp3").play();
-    document.getElementById("mp3").volume = 0.2;
-  };
-
 setTimeout(function() {
     Clock.init('canvas');
 }, 2000)
